6d343a505bc495699a365fe64a654d64b57cfa75
Joyal910 <156289919+Joyal910@users.noreply.github.com>
2024-07-14 14:39:06 +0530

    Update register.php

--- github/register.php
+++ github/register.php
@@ -6,76 +6,95 @@
     <title>Car Rental Registration</title>
     <link rel="stylesheet" href="assets/register/u login.css">
     <script>
-        function validateForm() {
-            var firstname = document.forms["registrationForm"]["firstname"].value;
-            var lastname = document.forms["registrationForm"]["lastname"].value;
-            var email = document.forms["registrationForm"]["email"].value;
-            var phone = document.forms["registrationForm"]["phone"].value;
-            var username = document.forms["registrationForm"]["username"].value;
-            var password = document.forms["registrationForm"]["password"].value;
-            var confirm_password = document.forms["registrationForm"]["confirm_password"].value;
-
-            // Basic validation - checking if fields are empty
-            if (firstname == "" || lastname == "" || email == "" || phone == "" || username == "" || password == "" || confirm_password == "") {
-                alert("All fields must be filled out");
-                return false;
+    function checkAvailability(type, value) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                if (xhr.responseText == "taken") {
+                    document.getElementById(type + "-error").innerText = "Already taken";
+                } else {
+                    document.getElementById(type + "-error").innerText = "";
+                }
             }
+        };
+        
+        xhr.send("type=" + type + "&value=" + value);
+    }
 
-            // Phone number validation
-            if (!/^(9|7|6)\d{8,9}$/.test(phone)) {
-                alert("Phone number should start with 9, 7, or 6 and be 9 to 10 digits long");
-                return false;
-            }
+    function validateForm() {
+        var firstname = document.forms["registrationForm"]["firstname"].value;
+        var lastname = document.forms["registrationForm"]["lastname"].value;
+        var email = document.forms["registrationForm"]["email"].value;
+        var phone = document.forms["registrationForm"]["phone"].value;
+        var username = document.forms["registrationForm"]["username"].value;
+        var password = document.forms["registrationForm"]["password"].value;
+        var confirm_password = document.forms["registrationForm"]["confirm_password"].value;
 
-            // Password validation - basic check for length
-            if (password.length < 6) {
-                alert("Password must be at least 6 characters long");
-                return false;
-            }
-u
-            // Checking if passwords match
-            if (password !== confirm_password) {
-                alert("Passwords do not match");
-                return false;
-            }
-
-            // You can add more specific validations here if needed, such as email format validation, etc.
-
-            // If all validations pass, return true to allow form submission
-            return true;
+        // Basic validation - checking if fields are empty
+        if (firstname == "" || lastname == "" || email == "" || phone == "" || username == "" || password == "" || confirm_password == "") {
+            alert("All fields must be filled out");
+            return false;
         }
-    </script>
+
+        // Phone number validation
+        if (!/^(9|7|6)\d{8,9}$/.test(phone)) {
+            alert("Phone number should start with 9, 7, or 6 and be 9 to 10 digits long");
+            return false;
+        }
+
+        // Password validation - basic check for length
+        if (password.length < 6) {
+            alert("Password must be at least 6 characters long");
+            return false;
+        }
+
+        // Checking if passwords match
+        if (password !== confirm_password) {
+            alert("Passwords do not match");
+            return false;
+        }
+
+        // If all validations pass, return true to allow form submission
+        return true;
+    }
+</script>
+
 </head>
 <body class="register-page">
     <div>
          <h2>Register to Forza Motors</h2>
      
-        <form name="registrationForm" method="post" onsubmit="return validateForm()">
-            
-           <div class="textbox">
-                <input type="text" placeholder="First Name" name="firstname" required>
-            </div>
-            <div class="textbox">
-                <input type="text" placeholder="Last Name" name="lastname" required>
-            </div>
-            <div class="textbox">
-                <input type="email" placeholder="Email" name="email" required>
-            </div>
-            <div class="textbox">
-                <input type="tel" placeholder="Phone Number" name="phone" required>
-            </div>
-            <div class="textbox">
-                <input type="text" placeholder="Username" name="username" required>
-            </div>
-            <div class="textbox">
-                <input type="password" placeholder="Password" name="password" required>
-            </div>
-            <div class="textbox">
-                <input type="password" placeholder="Confirm Password" name="confirm_password" required>
-            </div>
-            <input type="submit" class="btn" value="Register">
-            <p class="login-link"><center>Already have an account?<a href="index.php">Login</a></center></p>
-        </form>
+         <form name="registrationForm" method="post" onsubmit="return validateForm()">
+    <div class="textbox">
+        <input type="text" placeholder="First Name" name="firstname" required>
+    </div>
+    <div class="textbox">
+        <input type="text" placeholder="Last Name" name="lastname" required>
+    </div>
+    <div class="textbox">
+        <input type="email" placeholder="Email" name="email" required onkeyup="checkAvailability('email', this.value)">
+        <span id="email-error" style="color: red;"></span>
+    </div>
+    <div class="textbox">
+        <input type="tel" placeholder="Phone Number" name="phone" required onkeyup="checkAvailability('phone', this.value)">
+        <span id="phone-error" style="color: red;"></span>
+    </div>
+    <div class="textbox">
+        <input type="text" placeholder="Username" name="username" required>
+    </div>
+    <div class="textbox">
+        <input type="password" placeholder="Password" name="password" required>
+    </div>
+    <div class="textbox">
+        <input type="password" placeholder="Confirm Password" name="confirm_password" required>
+    </div>
+    <input type="submit" class="btn" value="Register">
+    <p class="login-link"><center>Already have an account?<a href="index.php">Login</a></center></p>
+</form>
+
     </div>
     
     <?php
@@ -103,6 +122,9 @@ u
     
     // Retrieve form data
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+
         $firstname = sanitize_input($_POST['firstname']);
         $lastname = sanitize_input($_POST['lastname']);
         $email = sanitize_input($_POST['email']);
@@ -119,8 +141,8 @@ u
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     
             // SQL query to insert data into database
-            $sql = "INSERT INTO users_data (firstname, lastname, email, phoneno, username, password,confirm_password) 
-                    VALUES ('$firstname', '$lastname', '$email', '$phone', '$username', '$hashed_password','$password')";
+            $sql = "INSERT INTO users_data (firstname, lastname, email, phoneno, username, password,confirm_password,role) 
+                    VALUES ('$firstname', '$lastname', '$email', '$phone', '$username', '$hashed_password','$password',"2")";
     
             // Execute query
             if ($conn->query($sql) === TRUE) {
@@ -128,6 +150,29 @@ u
             } else {
                 echo "Error: " . $sql . "<br>" . $conn->error;
             }
+        }
+        {
+            $type = $_POST['type'];
+            $value = $_POST['value'];
+        
+            if ($type == "email") {
+                $sql = "SELECT * FROM users_data WHERE email = ?";
+            } else if ($type == "phone") {
+                $sql = "SELECT * FROM users_data WHERE phoneno = ?";
+            }
+        
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $value);
+            $stmt->execute();
+            $stmt->store_result();
+        
+            if ($stmt->num_rows > 0) {
+                echo "taken";
+            } else {
+                echo "available";
+            }
+        
+            $stmt->close();
         }
     }
     
